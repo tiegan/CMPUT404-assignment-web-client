8f6162aa51d998f62d92999657b04729d9aae261
tiegan <tiegan@ualberta.ca>
2017-02-05 21:33:56 -0700

    Assignment 2 done

--- README.md
+++ README.md
@@ -13,8 +13,29 @@ Contributors / Licensing
 Generally everything is LICENSE'D under the Apache 2 license by Abram Hindle, 
 https://github.com/tywtyw2002, and https://github.com/treedust
 
+httpclient.py contains contributions from:
+
+* Abram Hindle
+* Tiegan Bonowicz
+* https://github.com/tywtyw2002
+* https://github.com/treedust
+
 But the server.py example is derived from the python documentation
 examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
 
+References
+========================
+URL: https://docs.python.org/2/library/urllib.html  
+Title: "20.5. urllib — Open arbitrary resources by URL"  
+Date visited: February 4, 2017  
+Copyright © 1990-2017, Python Software Foundation; All Rights Reserved
+under the PSF license (GPL compatible)
+
+
+URL: https://docs.python.org/2/library/urlparse.html  
+Title: "20.16. urlparse — Parse URLs into components"  
+Date visited: February 4, 2017  
+Copyright © 1990-2017, Python Software Foundation; All Rights Reserved
+under the PSF license (GPL compatible)
--- httpclient.py
+++ httpclient.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
-# Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
+# Copyright 2016 Abram Hindle, https://github.com/tywtyw2002,
+#                https://github.com/treedust and Tiegan Bonowicz
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,31 +24,46 @@ import socket
 import re
 # you may use urllib to encode data appropriately
 import urllib
+import urlparse
 
 def help():
     print "httpclient.py [GET/POST] [URL]\n"
 
 class HTTPResponse(object):
-    def __init__(self, code=200, body=""):
+    def __init__(self, code=500, body=""):
         self.code = code
         self.body = body
 
+# Stuff starts being changed here.
 class HTTPClient(object):
-    #def get_host_port(self,url):
 
+    # Lab 2 all over again. Use the port (or 80 if not given) to connect to
+    # the given host.
     def connect(self, host, port):
-        # use sockets!
-        return None
+        if port is None:
+            port = 80
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        clientSocket.connect((host, port))
+        return clientSocket
 
+    # Get the code by splitting the response and taking the first
     def get_code(self, data):
-        return None
+        try:
+            code = int(data.split(" ")[1])
+        except:
+            code = 404
+        return code
 
+    # It's not used so it doesn't get changed.
     def get_headers(self,data):
         return None
 
+    # Return the body by splitting the data by the two line breaks then taking
+    # the second part of the split.
     def get_body(self, data):
-        return None
+        return data.split("\r\n\r\n", 1)[1]
 
+    # This remains the same.
     # read everything from the socket
     def recvall(self, sock):
         buffer = bytearray()
@@ -60,17 +76,85 @@ class HTTPClient(object):
                 done = not part
         return str(buffer)
 
+    # Get websites.
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        # Use urlparse to split the url up for convenience
+        parseResult = urlparse.urlparse(url)
+        host = parseResult.hostname # i.e. "www.google.ca"
+        port = parseResult.port # i.e. "8080"
+        path = parseResult.path # i.e. "/abc/def"
+
+        # Get the client to connect using the host and port.
+        clientSocket = self.connect(host, port)
+
+        # GET header.
+        request = "GET %s HTTP/1.1\r\n" %path
+        request += "Host: %s\r\n" %host
+        request += "Accept: */*\r\n\r\n"
+
+        # Use the socket to send the request, get the response from recvall
+        # then close the socket as it's done its job.
+        clientSocket.sendall(request)
+        response = self.recvall(clientSocket)
+        clientSocket.close()
+
+        # Print the response, get the code/body and return it.
+        print(response)
+        code = self.get_code(response)
+        body = self.get_body(response)
         return HTTPResponse(code, body)
 
+    # I think like 75% of this is repeated from the GET function. I really
+    # should just make a function that can handle both to reduce redundancies
+    # but eh, it's not required for this assignment.
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        # Use urlparse to split the url up for convenience
+        parseResult = urlparse.urlparse(url)
+        host = parseResult.hostname
+        port = parseResult.port
+        path = parseResult.path
+        input = ""
+        contentLength = 0
+
+        # If we have args to use in the POST then encode them into input and
+        # get the length.
+        if(args != None):
+            input = urllib.urlencode(args)
+            contentLength = len(input)
+
+        # Get the client to connect using the host and port.
+        clientSocket = self.connect(host, port)
+
+        # POST header + input.
+        request = "POST %s HTTP/1.1\r\n" %path
+        request += "Host: %s\r\n" %host
+        request += "Accept: */*\r\n"
+        request += "Content-Type: application/x-www-form-urlencoded\r\n"
+        request += "Content-Length: %d\r\n\r\n" %contentLength
+        
+        # Good idea to add the if just so we don't have yet another \r\n with
+        # no input.
+        if(input != ""):
+            request += input + "\r\n"
+
+        # Use the socket to send the request, get the response from recvall
+        # then close the socket as it's done its job.
+        clientSocket.sendall(request)
+        response = self.recvall(clientSocket)
+        clientSocket.close()
+
+        # Print the response, get the code/body and return it.
+        print(response)
+        code = self.get_code(response)
+        body = self.get_body(response)
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
+        # Need to add HTTP to the url if necessary, else it just goes "lol nope"
+        if(url[0:5] != "http"):
+            url = "http://" + url
+
+        # Everything below is left the same.
         if (command == "POST"):
             return self.POST( url, args )
         else:
